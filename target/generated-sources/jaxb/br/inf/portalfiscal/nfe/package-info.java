f7e294357d445e4235f451780e2d7068e6d65a93
bronx92 <especialcontas@outlook.com>
2022-05-09 22:01:24 -0300

    Corrige .gitignore

--- target/generated-sources/jaxb/br/inf/portalfiscal/nfe/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.2 
-// Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2022.05.09 às 09:33:43 PM BRT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.portalfiscal.inf.br/nfe", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package br.inf.portalfiscal.nfe;
